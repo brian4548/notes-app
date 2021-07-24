49cc01fd9c3201d37cdbbfb2280e00d5185bbf21
brian4548 <brianr4548@gmail.com>
2021-07-24 16:41:44 -0500

    yargs commands with place holders

--- notes-app/App.js
+++ notes-app/App.js
@@ -1,9 +1,61 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-console.log(getNotes())
+yargs.version('1.1.0')
 
-const inverse = chalk.red.inverse
 
-console.log(inverse('hello'))
+// add notes, removes notes, read notes
 
+
+
+//add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function () {
+        console.log('adding a new note')
+    }
+})
+
+// remove command
+yargs.command({
+    command: 'remove',
+    describe: 'removes a note',
+    handler: function () {
+        console.log('removes note')
+    }
+})
+
+// list command
+yargs.command({
+    command: 'list',
+    describe: 'lists the notes',
+    handler: function () {
+        console.log('listing notes')
+    }
+})
+
+// read command
+yargs.command({
+    command: 'read',
+    describe: 'reads notes',
+    handler: function () {
+        console.log('reading notes')
+    }
+})
+
+
+
+
+
+console.log(yargs.argv)
+// const command = process.argv[2]
+// console.log(process.argv)
+
+// if (command === 'add') {
+//     console.log('adding notes')
+
+// } else if (command === 'remove') {
+//     console.log('remove note')
+// }
